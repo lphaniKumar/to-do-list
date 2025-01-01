5a724b003d7e2bb7cd6eb62e95b093f9b30b396a
Phani <lphanikumar@yahoo.co.in>
2025-01-01 15:40:41 +0100

    documentdb2

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '@/amplify/data/resource';
+
+const client = generateClient<Schema>();
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const { data } = await client.models.Todo.list();
+      setTodos(data);
+    };
+
+    fetchTodos();
+  }, []);
+
+  const deleteTodo = async (id: string) => {
+    await client.models.Todo.delete({ id });
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.content}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
